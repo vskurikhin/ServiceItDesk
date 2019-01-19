26e021101ac458f801f2f21238af623f2774caff
VSkurikhin <stalker@quake.ru>
2019-01-19 18:45:22 +0300

    second commit create branch Week-1

---
+++ src/main/java/su.svn/CreateTables.java
@@ -0,0 +1,23 @@
+package su.svn;
+
+/*
+ * Created by VSkurikhin at 2019.
+ */
+
+/**
+ *
+ * mvn clean compile dependency:copy-dependencies
+ * createSchema.sh or createSchema.bat
+ */
+
+public class CreateTables {
+    public static final String DB_SCHEMA_DDL = "db-schema.hibernate5.ddl";
+
+    public static void main(String[] args) {
+        // createTables();
+    }
+}
+
+/* vim: syntax=java:fileencoding=utf-8:fileformat=unix:tw=78:ts=4:sw=4:sts=4:et
+ */
+//EOF
---
+++ src/test/java/su.svn/TestData.java
@@ -0,0 +1,5 @@
+package su.svn;
+
+public class TestData
+{
+}
