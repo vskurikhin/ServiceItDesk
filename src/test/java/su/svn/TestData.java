7198bb956f24e941a3044f13b9c7d9a5cec4dac8
VSkurikhin <stalker@quake.ru>
2019-01-25 23:29:15 +0300

    version 1.0-13.

--- src/test/java/su/svn/TestData.java
+++ src/test/java/su/svn/TestData.java
@@ -19,6 +19,7 @@ import static org.mockito.Mockito.mock;
 @FieldNameConstants
 public class TestData
 {
+    public static final String VERSION = "1";
     public static final String TEST_STR1 = "TEST_STR1";
     public static final String TEST_STR2 = "TEST_STR2";
     public static final String TEST_STR3 = "TEST_STR3";
